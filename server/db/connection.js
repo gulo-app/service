589639e4b84bce7a675ed6df3a0ffd9ba253f8e1
flomermer <flom.tomer@gmail.com>
2019-04-01 21:42:32 +0300

    change google mysql-zone europe

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql');
+const IS_PROD  =  process.env.IS_PROD;
+
 const conn  = mysql.createPool({
-  host: '35.226.42.229', //process.env.isProd ? '35.226.42.229' : 'localhost',
+  host: IS_PROD ? '35.205.128.46' : 'localhost', //process.env.isProd ? '35.226.42.229' : 'localhost',
   user: "gulo",
   password: "shenkar",
   database: "gulo",
@@ -8,6 +10,10 @@ const conn  = mysql.createPool({
   supportBigNumbers: true,
   multipleStatements: true
 });
+
+if(IS_PROD){
+  console.log("running on remote mySQL");
+}
 
 conn.sql = async (sql) => {
   return new Promise((resolve,reject) => {

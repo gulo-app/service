f4427f5f08158c9da8165525260a7b434d0288a5
flomermer <flom.tomer@gmail.com>
2019-04-04 14:49:16 +0300

    montv-prod fix socket

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = async () => {
       var ssl_credentials = {key:  fs.readFileSync(`../../nodejs/montv-service/server/security/ssl/montv.pem`, 'utf8'), cert: fs.readFileSync(`../../nodejs/montv-service/server/security/ssl/montv.cer`, 'utf8')};
 
       const httpsServer = https.createServer(ssl_credentials, app);
-      await socket(httpServer, app);
+      await socket(httpsServer, app);
       httpsServer.listen(PORT, () => {
        console.log(`listening on https:${PORT}`);
       });

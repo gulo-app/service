37ed793d446f0453f0771b1423660b4f2795d2fb
flomermer <flom.tomer@gmail.com>
2019-05-19 01:02:54 +0300

    service serves react-build

---
+++ client/_public/build/precache-manifest.ff40fd2087e2df594951f801ba076d06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4b2d59684dd89b2829e9",
+    "url": "/static/js/main.110a5944.chunk.js"
+  },
+  {
+    "revision": "3c19318c4903461ec5b5",
+    "url": "/static/js/2.077026a8.chunk.js"
+  },
+  {
+    "revision": "4b2d59684dd89b2829e9",
+    "url": "/static/css/main.3fb99aea.chunk.css"
+  },
+  {
+    "revision": "c9789c4764cf68eabb5288ddc238aa60",
+    "url": "/index.html"
+  }
+];
--- client/_public/build/service-worker.js
+++ client/_public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7995ea0c187c797b0afffc9d473bb00c.js"
+  "/precache-manifest.ff40fd2087e2df594951f801ba076d06.js"
 );
 
 workbox.clientsClaim();

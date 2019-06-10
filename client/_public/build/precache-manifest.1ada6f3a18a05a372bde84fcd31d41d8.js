189eeaf105a2f090b3cf0380adb0334209a77e66
flomermer <flom.tomer@gmail.com>
2019-06-10 13:05:49 +0300

    fix facebook reLogin bug

---
+++ client/_public/build/precache-manifest.1ada6f3a18a05a372bde84fcd31d41d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d33ade1bb2aec1befc86c51913add4c",
+    "url": "/static/media/logo.4d33ade1.png"
+  },
+  {
+    "revision": "a71287b6208aba540472f58d3e6db77e",
+    "url": "/static/media/google.a71287b6.png"
+  },
+  {
+    "revision": "c04bef45c0aa8661523f1ed6434ff58a",
+    "url": "/static/media/facebook.c04bef45.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "672cdac25e111de13c1e",
+    "url": "/static/js/main.1cd677b8.chunk.js"
+  },
+  {
+    "revision": "f1ecd242d50b6103888b",
+    "url": "/static/js/2.240dff09.chunk.js"
+  },
+  {
+    "revision": "672cdac25e111de13c1e",
+    "url": "/static/css/main.c48e8840.chunk.css"
+  },
+  {
+    "revision": "3f984a13aca48b5d522bd7e8f2eb70aa",
+    "url": "/index.html"
+  }
+];
--- client/_public/build/service-worker.js
+++ client/_public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.075502425d2c9dbeeed23ec413442f5f.js"
+  "/precache-manifest.1ada6f3a18a05a372bde84fcd31d41d8.js"
 );
 
 workbox.clientsClaim();

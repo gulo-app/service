a8a53a262cf6500c3e555cf44a6c9ca5de6f0650
flomermer <flom.tomer@gmail.com>
2019-06-15 05:46:53 +0300

    viewList added platform state

---
+++ client/_public/build/precache-manifest.fd60080362a68a0d455436b98ab6f159.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd2b6ba934527618a030ae63a6d1aadf",
+    "url": "/static/media/logo.fd2b6ba9.png"
+  },
+  {
+    "revision": "a71287b6208aba540472f58d3e6db77e",
+    "url": "/static/media/google.a71287b6.png"
+  },
+  {
+    "revision": "c04bef45c0aa8661523f1ed6434ff58a",
+    "url": "/static/media/facebook.c04bef45.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "fbc11ac837f74e638af6",
+    "url": "/static/js/main.2ec13019.chunk.js"
+  },
+  {
+    "revision": "95bdaa41ae9dfcb8e048",
+    "url": "/static/js/2.aef176b6.chunk.js"
+  },
+  {
+    "revision": "fbc11ac837f74e638af6",
+    "url": "/static/css/main.e80064d8.chunk.css"
+  },
+  {
+    "revision": "7f5ec2cdfcdb6fe48cddaf60487a3e6c",
+    "url": "/index.html"
+  }
+];
--- client/_public/build/service-worker.js
+++ client/_public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb60749e9dc2a4ecb497908c4cc03655.js"
+  "/precache-manifest.fd60080362a68a0d455436b98ab6f159.js"
 );
 
 workbox.clientsClaim();

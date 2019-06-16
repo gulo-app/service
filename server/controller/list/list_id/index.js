e2c4f88cd0dd107ad4bd2d699c3c39988b2dbbec
flomermer <flom.tomer@gmail.com>
2019-06-16 17:09:08 +0300

    fix shoppingCart firm.products Iterable bug

--- server/controller/list/list_id/index.js
+++ server/controller/list/list_id/index.js
@@ -174,6 +174,10 @@ const getBestShoppingCart = async (list_id) => {
   for(let firm of firms){
     firm.products         =   firm_products[firm.shopping_cart_firm_id];
     firm.total_price      =   _.round(_.sumBy(firm.products, 'price'),2).toFixed(2);
+
+    if(!firm.products)
+      continue;
+
     for(let p of firm.products)
       p.price = _.round(p.price, 2).toFixed(2);
   }

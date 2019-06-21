fddca61835a2805621ed6f3233dafeff1e7d59de
flomermer <flom.tomer@gmail.com>
2019-06-21 04:00:01 +0300

    test

--- server/middleware/passport/index.js
+++ server/middleware/passport/index.js
@@ -9,12 +9,10 @@ const fbAdmin             =     require('../../firebase');
 
 module.exports = () => {
   passport.use('firebase', new CustomStrategy(async function(req, done) {
-      console.log(`test`);
       let {idToken, email} = req.body;
-      console.log(req.body);
       if(!idToken)
         return done('idToken is missing', null);
-      console.log(idToken);
+
       const user = await verifyGoogleTokenByFirebaseAdmin(idToken);
       if(!user || !user.email)
         return done('idToken is missing', null);

0f97a0a3ddbdc825d23444a393f24b390c4c7d5a
flomermer <flom.tomer@gmail.com>
2019-07-21 14:57:24 +0300

    remove console on login

--- server/routes/user/login/index.js
+++ server/routes/user/login/index.js
@@ -31,35 +31,6 @@ router.post('/byAuthToken', (req, res) => {
   })(req, res);
 });
 
-
-// router.post('/google', (req, res) => {
-//   passport.authenticate('googleID', (err, user) => {
-//     if(err)
-//       return res.status(500).send(err);
-//     if(!user)
-//       return res.status(500).send('Credentials Inavlid');
-//
-//     req.login(user, (err) => {
-//       return res.send(user);
-//     })
-//   })(req, res);
-// });
-//
-// router.post('/facebook', (req, res) => {
-//   try{
-//     passport.authenticate('facebookID', (err, user) => {
-//       if(err)
-//         return res.status(500).send(err);
-//       if(!user)
-//         return res.status(500).send('Credentials Inavlid');
-//
-//       req.login(user, (err) => {
-//         return res.send(user);
-//       })
-//     })(req, res);
-//   }catch(e){RES_ERROR(res, e)};
-// });
-
 router.post('/auth-test', auth, (req,res) => {
   res.send(req.user);
 })
